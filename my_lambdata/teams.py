6e936ca96f59c6d8ba5f34d8a858aa3235031b9f
ChicagoDataScientist <sonotony@gmail.com>
2020-03-19 19:59:04 -0500

    OOP

--- my_lambdata/teams.py
+++ my_lambdata/teams.py
@@ -5,31 +5,39 @@ class Team():
         self.name = name
         self.city = city
 
-def advertise(my_team):
-    print(f"HEY COME TO {my_team['city'].upper()} TO SEE OUR GAMES!!!")
-def full_name(my_team):
-    return f"{my_team['city']} {my_team['name']}"
+    def advertise(self):
+        print(f"HEY COME TO {self.city.upper()} TO SEE OUR GAMES!!!")
+
+    def full_name(self):
+        return f"{self.city} {self.name}"
+
 if __name__ == "__main__":
-    # teams = [
-    #     {"city": "New York", "name": "Yankees"},
-    #     {"city": "New York", "name": "Mets"},
-    #     {"city": "Boston", "name": "Red Sox"},
-    #     {"city": "New Haven", "name": "Ravens"},
-    #     {"city": "Washington", "name": "Nationals"}
-    # ]
-    # for team in teams:
-    #     print("-------")
-    #     print(full_name(team))
+    teams = [
+        {"city": "New York", "name": "Yankees"},
+        {"city": "New York", "name": "Mets"},
+        {"city": "Boston", "name": "Red Sox"},
+        {"city": "New Haven", "name": "Ravens"},
+        {"city": "Washington", "name": "Nationals"}
+    ]
+    for team_attributes in teams:
+        team = Team(name=team_attributes["name"], city=team_attributes["city"])
+        print("-------")
+        # print(full_name(team))
+        print(team.city)
+        print(team.full_name())
+        team.advertise()
+
+
     #     advertise(team)
 
-    team = Team(city="Washington", name="Wizards") # Initialize / create an instance of the object
-    print (team)
-    print(type(team))
-    print(team.name) # invoking attributes
-    print(team.city) # invoking attributes
+    # team = Team(city="Washington", name="Wizards") # Initialize / create an instance of the object
+    # print (team)
+    # print(type(team))
+    # print(team.name) # invoking attributes
+    # print(team.city) # invoking attributes
 
-    team2 = Team("Giants", "New York") # Initialize / create an instance of the object
-    print (team2)
-    print(type(team2))
-    print(team2.name) # invoking attributes
-    print(team2.city) # invoking attributes
+    # team2 = Team("Giants", "New York") # Initialize / create an instance of the object
+    # print (team2)
+    # print(type(team2))
+    # print(team2.name) # invoking attributes
+    # print(team2.city) # invoking attributes
